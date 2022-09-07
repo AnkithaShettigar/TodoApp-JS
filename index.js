5a9432644cd3cf4532c0a9138b53c28f042b5835
AnkithaShettigar <107766536+AnkithaShettigar@users.noreply.github.com>
2022-09-07 14:27:49 +0530

    Added TodoApp-JS

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function toggle(){
 }
 function addTask(){
     popup.classList.toggle('active'); 
-    document.getElementsByClassName('container')[0].style.filter = "blur(5px)";
+    document.getElementsByClassName('container')[0].style.filter = "blur(0px)";
     
 }
 
